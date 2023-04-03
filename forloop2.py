24250e50b1e898b0ae978f47d74ca5e89af28224
Md Affan Hossain <125567188+mdaffan-csegis@users.noreply.github.com>
2023-04-03 10:36:50 +0600

    Add files via upload

---
+++ forloop2.py
@@ -0,0 +1,37 @@
+
+""""
+
+# 1+2+3+4+5........10
+
+result = 0
+
+for i in range(11):
+
+    result = result +i
+    #result+=1
+
+
+
+print(result)
+
+"""""
+
+# While loop
+"""""
+i = 0
+
+while i<=30 :
+    print(i)
+
+    i= i+1
+    #i++
+
+
+"""""
+
+i = 1
+
+while i<=30 :
+    print(i)
+
+    i= i+2
---
+++ function.py
@@ -0,0 +1,41 @@
+
+""""
+
+def fruits():
+    print("I love eating fruits")
+
+
+fruits()
+
+"""""
+
+
+
+def add(x,y):
+
+    result = x+y
+    return result
+
+def sub(x,y):
+
+    result = x-y
+    return result
+
+
+
+
+
+num1 = 40
+num2 = 20
+
+result = add(num1,num2)
+result2 = sub(num1,num2)
+
+print(result)
+print(result2)
+
+
+
+
+
+
