7fe0a0919d38a2fd858637479e5fb1fecceb6062
Md Affan Hossain <125567188+mdaffan-csegis@users.noreply.github.com>
2023-05-22 21:40:06 +0600

    Add files via upload

---
+++ practice_7.py
@@ -0,0 +1,32 @@
+#for loop
+
+""""
+for i in range(10) :
+    print("I am learning for loop")
+
+
+    #2+4+6+8......100
+    #2,4,8,16......n
+
+    # id n =10 then 
+
+
+
+"""
+"""
+for _ in range(1,30,2) :
+    print(_)
+
+
+"""
+"""
+fruits =["apple","bananna","mango"]
+
+for fruit1 in fruits :
+    print(fruit1)
+"""
+
+
+for i in "Affan" :
+    print(i)
+    
